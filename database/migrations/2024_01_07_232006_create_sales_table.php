bb717fabc3175b0595381af6f0574518de214cb0
Carlos Del Castillo M <maussac13@gmail.com>
2024-01-07 18:25:04 -0500

    creation model Sale, migrate and factory

---
+++ database/migrations/2024_01_07_232006_create_sales_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('total', 11, 2);
+            $table->decimal('pago', 11, 2)->nullable();
+            $table->date('fecha');
+            $table->foreign('user_id')->constrained();
+            $table->foreign('client_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
