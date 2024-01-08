46a1fcd6afc4bf64045f4e2b0b3d5f2bd6c23385
Carlos Del Castillo M <maussac13@gmail.com>
2024-01-08 17:33:45 -0500

    implementacion de componente livewire para categorias

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Category\CategoryComponent;
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Home\Inicio;
 /*
@@ -22,3 +23,4 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/inicio', Inicio::class)->name('inicio');
+Route::get('/categories', CategoryComponent::class)->name('inicio');
