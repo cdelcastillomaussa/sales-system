4ff230a43a356f1686cc6c51fa3e623f2d5dd23c
Carlos Del Castillo M <maussac13@gmail.com>
2024-01-08 16:36:50 -0500

    implementando tools a las cards de bootstrap

--- resources/views/livewire/home/inicio.blade.php
+++ resources/views/livewire/home/inicio.blade.php
@@ -1,6 +1,9 @@
 <div>
     <h1>Componente Inicio</h1>
-    <x-card cardTitle="Card Title" cardTools="Card Tools" cardFooter="Card Footer">
+    <x-card cardTitle="Card Title" cardFooter="Card Footer">
+        <x-slot:cardTools>
+            <a href="#" class="btn btn-primary small btn-sm">Crear</a>
+        </x-slot:cardTools>
         Contenido
     </x-card>
 </div>
