4df4ad13eefb221fbe430520056cf93b46d7084a
Carlos Del Castillo M <maussac13@gmail.com>
2024-01-08 17:18:52 -0500

    implementacion de componente para las tablas

--- resources/views/livewire/home/inicio.blade.php
+++ resources/views/livewire/home/inicio.blade.php
@@ -3,7 +3,23 @@
     <x-card cardTitle="Card Title" cardFooter="Card Footer">
         <x-slot:cardTools>
             <a href="#" class="btn btn-primary small btn-sm">Crear</a>
-        </x-slot:cardTools>
-        Contenido
+        </x-slot>
+
+        <x-table>
+            <x-slot:thead>
+                <th>thead</th>
+                <th>thead</th>
+            </x-slot>
+
+            <tr>
+                <td>...</td>
+                <td>...</td>
+            </tr>
+
+            
+            
+        </x-table>
+
     </x-card>
+
 </div>
