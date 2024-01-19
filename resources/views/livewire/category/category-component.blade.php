1551425111de0be58aaf9e53f8ce35f2fbffb186
Carlos Del Castillo M <maussac13@gmail.com>
2024-01-19 15:28:27 -0500

    Reestrcuturacion del modal para el componente categories

--- resources/views/livewire/category/category-component.blade.php
+++ resources/views/livewire/category/category-component.blade.php
@@ -50,19 +50,5 @@
         </x-slot>
     </x-card>
 
-    <x-modal modalId="modalCategory" modalTitle="Categorias">
-        <form wire:submit={{ $Id == 0 ? "store" : "update($Id)" }}>
-            <div class="form-row">
-                <div class="form-group col-12">
-                    <label for="name">Nombre:</label>
-                    <input wire:model='name' type="text" class="form-control" placeholder="Nombre categoria" id="name">
-                    @error('name')
-                        <div class="alert alert-danger w-100 mt-2">{{ $message }}</div>
-                    @enderror
-                </div>
-            </div>
-            <hr>
-            <button type="submit" class="btn btn-primary float-right">{{ $Id == 0 ? 'Guardar' : 'Editar' }}</button>
-        </form>
-    </x-modal>
+    @include('categories.modal')
 </div>
